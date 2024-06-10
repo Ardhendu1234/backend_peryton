dfa13006c7d7c2e643f16ff088046d8541d945fb
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-10 12:58:35 +0530

    utils files are added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+
+const asyncHandler=(fn)=>{
+    return (req,res,next)=>{
+        Promise.resolve(fn(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export { asyncHandler };
+
+//trycatch method
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+*/
+
+
+
+//all other way of thinking this
+// const asyncHandler=()=>{}
+// const asyncHandler=(fn)=>{()=>{}}
+// const asyncHandler=(fn)=> {async()=>{}}
