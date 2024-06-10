18c95ac7286e181889a79c330c1a5f67587554ee
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-10 12:44:09 +0530

    data base is connected and server is listening

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB=async()=>{
+    try {
+       const connection= await mongoose.connect(process.env.MONGO_URI,{
+        dbName:DB_NAME
+       })
+       console.log(`MongoDb is connected : ${connection.connection.host}`)
+    } catch (error) {
+        console.log("Failed to connect the DataBase:",error)
+        process.exit(1);
+    }
+}
+
+export default connectDB
