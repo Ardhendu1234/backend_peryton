88cbad9536d7e6f03a6ca8c4e77c2dc0dbb65ff3
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-10 15:34:44 +0530

    user route is created

---
+++ src/routes/user.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { logOutUser, loginUser, registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router=Router();
+
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/register").post(registerUser)
+router.route("/logout").post(verifyJWT,logOutUser)
+
+
+export default router
